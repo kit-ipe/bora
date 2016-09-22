cb6679cf2a58b0f71d78bb2332dbeb85e1cd3e25
chanhoonseng <chanhoonseng3101@gmail.com>
2016-09-22 11:09:56 +0200

    Added a fix time period for last value.
    Signed-off-by: chanhoonseng <chanhoonseng3101@gmail.com>

--- core.py
+++ core.py
@@ -10,6 +10,7 @@ import re
 import datetime
 from shutil import copyfile
 from time import gmtime, strftime
+import time
 
 
 import tornado.escape
@@ -86,10 +87,13 @@ def fetchDataADEI():
         return
     
     cache_data = {}
+    print time.time()
+    curtime = int(time.time())
+    time_range = str((curtime-3600)) + "-" + str(curtime)
     for param in varname:
         print param
         dest = config['server'] + config['script']
-        url = dest + "?" + varname[param] + "&window=-1"
+        url = dest + "?" + varname[param] + "&window=" + time_range
         print url
         data = requests.get(url,
                             auth=(config['username'],
