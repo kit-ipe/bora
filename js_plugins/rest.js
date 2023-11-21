771fb5cdd545e10b313e2e31c373696e256b6b81
Nicholas Tan Jerome <nicholastanjerome@gmail.com>
2023-11-21 18:21:48 +0100

    cleaned up the code for rest.js
    Signed-off-by: Nicholas Tan Jerome <nicholastanjerome@gmail.com>

--- js_plugins/rest.js
+++ js_plugins/rest.js
@@ -5,61 +5,47 @@ function rest_set_query(id, url, value) {
     data.value = value;
     var json = JSON.stringify(data);
 
-    var xhr = new XMLHttpRequest(); //invoke a new instance of the XMLHttpRequest
-    xhr.onload = success_set; // call success function if request is successful
-    xhr.onerror = error;  // call error function if request failed
-    //xhr.open('GET', 'https://api.github.com/users/manishmshiva'); // open a GET request
-    xhr.open('PUT', url); // open a GET request
-    xhr.send(json); // send the request to the server.
+    var xhr = new XMLHttpRequest();
+    xhr.onload = success_set;
+    xhr.onerror = error;
+    xhr.open('PUT', url);
+    xhr.send(json);
 }
 
 // function to handle success
 function success() {
     var data = JSON.parse(this.responseText); //parse the string to JSON
-    console.log(data);
     $( "#" + data["device_parameter"] ).find( "img" ).attr("src","/static/commbit_green.svg");
     $( "#" + data["device_parameter"] ).find( "input" ).val(data["value"]);
 }
 
 function success_set() {
     var data = JSON.parse(this.responseText); //parse the string to JSON
-    console.log(data);
-    $( "#" + data["device_parameter"] ).find( "img" ).attr("src","/static/commbit_green.svg");
-    $( "#" + data["device_parameter"] ).find( "input" ).val(data["value"]);
+    $("#" + data["device_parameter"] ).find( "img" ).attr("src","/static/commbit_green.svg");
+    $("#" + data["device_parameter"] ).find( "input" ).val(data["value"]);
   
     $('[data-type="rest"]').each(function(){
-      // Do stuff with each div
-      console.log($(this).closest('div').attr('id'));
-      console.log($(this).closest('div').attr('data-url'));
-      console.log($(this).val());
-      var id = $(this).closest('div').attr('id');
-      var url = $(this).closest('div').attr('data-url');
-      rest_get_query(id, url);
+        var id = $(this).closest('div').attr('id');
+        var url = $(this).closest('div').attr('data-url');
+        rest_get_query(id, url);
     });
 }
 
-// function to handle error
 function error(err) {
     console.log('Request Failed', err); //error details will be in the "err" object
 }
 
-
 function rest_get_query(id, url) {
     $( "#" + id ).find( "img" ).attr("src","/static/commbit_red.svg");
-    
-    var xhr = new XMLHttpRequest(); //invoke a new instance of the XMLHttpRequest
-    xhr.onload = success; // call success function if request is successful
-    xhr.onerror = error;  // call error function if request failed
-    xhr.open('GET', url ); // open a GET request
-    xhr.send(); // send the request to the server.
+    var xhr = new XMLHttpRequest();
+    xhr.onload = success;
+    xhr.onerror = error;
+    xhr.open('GET', url );
+    xhr.send();
 }
 
-
 $("input").on('keypress',function(e) {
-    if(e.which == 13) {
-        console.log($(this).closest('div').attr('id'));
-        console.log($(this).closest('div').attr('data-url'));
-        console.log($(this).val());
+    if (e.which == 13) {
         var id = $(this).closest('div').attr('id');
         var url = $(this).closest('div').attr('data-url');
         var value = $(this).val();
@@ -67,15 +53,10 @@ $("input").on('keypress',function(e) {
     }
 });
 
-$( window ).on( "load", function() {
-  console.log("This is rest.js onload");
-  $('[data-type="rest"]').each(function(){
-    // Do stuff with each div
-    console.log($(this).closest('div').attr('id'));
-    console.log($(this).closest('div').attr('data-url'));
-    console.log($(this).val());
-    var id = $(this).closest('div').attr('id');
-    var url = $(this).closest('div').attr('data-url');
-    rest_get_query(id, url);
-  });
-} );
+$(window).on("load", function() {
+    $('[data-type="rest"]').each(function(){
+        var id = $(this).closest('div').attr('id');
+        var url = $(this).closest('div').attr('data-url');
+        rest_get_query(id, url);
+    });
+});
