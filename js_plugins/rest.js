6063aa8f53c741e4840d40a2e99558b416166d36
Nicholas Tan Jerome <nicholastanjerome@gmail.com>
2023-11-21 23:11:59 +0100

    Fixed rest plugin

--- js_plugins/rest.js
+++ js_plugins/rest.js
@@ -1,50 +1,51 @@
 function rest_set_query(id, url, value) {
-    var myurl = url + "?v=" + value;
+    $( "#" + id ).find( "img" ).attr("src","/static/commbit_red.svg");
 
-    $.ajax({
-        url: myurl,
-        type: 'GET',
-        crossDomain: true,
-        success: function (response) {
-            console.log(response);
-        },
-        error: function () {
-            console.log("Error.")
-        }
-    });
+    var data = {};
+    data.value = value;
+    var json = JSON.stringify(data);
+
+    var xhr = new XMLHttpRequest();
+    xhr.onload = success_set;
+    xhr.onerror = error;
+    xhr.open('PUT', url);
+    xhr.send(json);
 }
 
 // function to handle success
 function success() {
     var data = JSON.parse(this.responseText); //parse the string to JSON
-    console.log(data);
     $( "#" + data["device_parameter"] ).find( "img" ).attr("src","/static/commbit_green.svg");
     $( "#" + data["device_parameter"] ).find( "input" ).val(data["value"]);
 }
 
-// function to handle error
+function success_set() {
+    var data = JSON.parse(this.responseText); //parse the string to JSON
+    $("#" + data["device_parameter"] ).find( "img" ).attr("src","/static/commbit_green.svg");
+    $("#" + data["device_parameter"] ).find( "input" ).val(data["value"]);
+  
+    $('[data-type="rest"]').each(function(){
+        var id = $(this).closest('div').attr('id');
+        var url = $(this).closest('div').attr('data-url');
+        rest_get_query(id, url);
+    });
+}
+
 function error(err) {
     console.log('Request Failed', err); //error details will be in the "err" object
 }
 
-
 function rest_get_query(id, url) {
     $( "#" + id ).find( "img" ).attr("src","/static/commbit_red.svg");
-
-    var xhr = new XMLHttpRequest(); //invoke a new instance of the XMLHttpRequest
-    xhr.onload = success; // call success function if request is successful
-    xhr.onerror = error;  // call error function if request failed
-    //xhr.open('GET', 'https://api.github.com/users/manishmshiva'); // open a GET request
-    xhr.open('GET', 'http://localhost:18080/api/v1/dma/' + id ); // open a GET request
-    xhr.send(); // send the request to the server.
+    var xhr = new XMLHttpRequest();
+    xhr.onload = success;
+    xhr.onerror = error;
+    xhr.open('GET', url );
+    xhr.send();
 }
 
-
 $("input").on('keypress',function(e) {
-    if(e.which == 13) {
-        console.log($(this).closest('div').attr('id'));
-        console.log($(this).closest('div').attr('data-url'));
-        console.log($(this).val());
+    if (e.which == 13) {
         var id = $(this).closest('div').attr('id');
         var url = $(this).closest('div').attr('data-url');
         var value = $(this).val();
@@ -52,15 +53,10 @@ $("input").on('keypress',function(e) {
     }
 });
 
-$( window ).on( "load", function() {
-  console.log("This is rest.js onload");
-  $('[data-type="rest"]').each(function(){
-    // Do stuff with each div
-    console.log($(this).closest('div').attr('id'));
-    console.log($(this).closest('div').attr('data-url'));
-    console.log($(this).val());
-    var id = $(this).closest('div').attr('id');
-    var url = $(this).closest('div').attr('data-url');
-    rest_get_query(id, url);
+$(window).on("load", function() {
+    $('[data-type="rest"]').each(function(){
+        var id = $(this).closest('div').attr('id');
+        var url = $(this).closest('div').attr('data-url');
+        rest_get_query(id, url);
+    });
 });
-} );
