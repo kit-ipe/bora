86eaba1c12d8e991d75fcab69ec528f5fed4cd46
Nicholas Tan Jerome <nicholastanjerome@gmail.com>
2024-05-30 20:19:35 +0200

    Added a link to direct adei data plugin to adei page
    Signed-off-by: Nicholas Tan Jerome <nicholastanjerome@gmail.com>

--- js_plugins/adei.js
+++ js_plugins/adei.js
@@ -1,10 +1,6 @@
 $(function(){
 
 /** BORA-JS **/
-
-
-
-
 });
 
 /*
@@ -44,11 +40,17 @@ function parse_adei(key, value, timestamp, invalid) {
     if (data_smaller_than != undefined && value < data_smaller_than) {
         $("#" + key + "> .value").css('color', 'red');
         $("#" + key + "> .unit-name").css('color', 'red');
+    } else {
+        $("#" + key + "> .value").css('color', 'green');
+        $("#" + key + "> .unit-name").css('color', 'green');
     }
 
     if (data_larger_than != undefined && value > data_larger_than) {
         $("#" + key + "> .value").css('color', 'red');
         $("#" + key + "> .unit-name").css('color', 'red');
+    } else {
+        $("#" + key + "> .value").css('color', 'green');
+        $("#" + key + "> .unit-name").css('color', 'green');
     }
 
     if (data_exp != undefined && data_exp == "true") {
@@ -86,6 +88,7 @@ $(".databox").hover(function(){
     var output_query = "db_server="+urlParams.get('db_server')+"&db_name="+urlParams.get('db_name')+"&db_group="+urlParams.get('db_group')+"&db_mask="+urlParams.get('db_mask'); 
 
     if ($("#" + key).attr('data-trend') == "true") {
+        $('#'+key + '> .popup').remove();
         $('#'+ key).append('<span class="popup" style="top:0; left:250px;"><img src="' + data_adei + 'services/getimage.php?' + output_query + '&window='+ trend_time_start.toString() +'-'+ trend_time_end.toString() + '&frame_width=600&frame_height=400" width="600px" height="400px"/></span>');
         var key_left = parseInt($("#" + key).css("left"));
 	var pos_left = key_left + 850;
@@ -105,3 +108,18 @@ $(".databox").hover(function(){
         }
     }
 });
+
+function open_adei_page(key) {
+    var data_link_adei = $("#" + key).attr('data-link-adei');
+
+    var data_query = $("#" + key).attr('data-query');
+    data_query = data_query.split("?")[1];
+    const urlParams = new URLSearchParams("?"+data_query);
+    var output_url = "https://adei-katrin.kaas.kit.edu/adei/#module=graph&db_server="+urlParams.get('db_server')+"&db_name="+urlParams.get('db_name')+"&db_group="+urlParams.get('db_group')+"&db_mask="+urlParams.get('db_mask')+"&experiment=-&window=86400&module=graph&virtual=srctree&srctree=&infomod=legend";
+
+    if (data_link_adei != undefined && data_link_adei == "true") {    
+        window.open(output_url, '_blank');
+    } else {
+        return;
+    }
+}
