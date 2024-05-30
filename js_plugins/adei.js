f188fbf6a91a33e27aaa6f886efc0dfb57a9ed7a
Nicholas Tan Jerome <nicholastanjerome@gmail.com>
2024-05-30 17:28:53 +0200

    Added tooltip and invalid delta time to the data
    Signed-off-by: Nicholas Tan Jerome <nicholastanjerome@gmail.com>

--- js_plugins/adei.js
+++ js_plugins/adei.js
@@ -14,12 +14,27 @@ data-link-adei="{{ data['style'][key]['div']['data-link-adei'] }}"
 data-trend="{{ data['style'][key]['div']['data-trend']
 */
 
-function parse_adei(key, value) {
+function parse_adei(key, value, timestamp, invalid) {
+    //console.log(invalid); // invalid value is in milliseconds
+    
+    let currentTime = new Date();
+    let currentTimeMillis = currentTime.getTime();
+
     var data_decimal_numbers = parseInt($("#" + key).attr("data-decimal-numbers"));
     var data_smaller_than = $("#" + key).attr("data-smaller-than");
     var data_larger_than = $("#" + key).attr("data-larger-than");
     var data_exp = $("#" + key).attr("data-exp");
-    
+
+
+    //var timestamp = response[key]["timestamp"];
+    //var invalid = response[key]["invalid"];
+    //var value = response[key]["value"];
+
+    var delta = (currentTimeMillis - timestamp) / 1000.0;
+    // since delta is converted to seconds, we need to convert invalid to seconds as well
+    invalid = invalid / 1000.0;
+
+
     if (data_decimal_numbers != undefined && data_decimal_numbers > 0) {
         value = (Math.round(value * 100) / 100).toFixed(data_decimal_numbers);
     }
@@ -41,6 +56,13 @@ function parse_adei(key, value) {
             value = parseFloat(value).toExponential();
         }
     }
+
+    if (invalid != undefined && invalid < delta) {
+        $("#" + key + "> .value").css('color', 'grey');
+        $("#" + key + "> .unit-name").css('color', 'grey');
+    }
+
+    $("#"+key).attr('tooltip',  key + "\nDeltaTime: " + parseFloat(delta).toFixed(3) + " s (" + parseFloat(delta / 60.0).toFixed(3) + " min)");
 
     return value;
 }
